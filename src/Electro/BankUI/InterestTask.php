ecd690245cccc561596cf031aeb365d6e8a5206d
Keenan Yafiq <accbruh0@gmail.com>
2022-01-15 17:23:05 +0700

    Update InterestTask.php

--- src/Electro/BankUI/InterestTask.php
+++ src/Electro/BankUI/InterestTask.php
@@ -17,7 +17,7 @@ class InterestTask extends Server {
         $this->plugin = $plugin;
     }
 
-    public function onRun($tick) : int
+    public function onRun($tick) : bool
     {
         $this->plugin->dailyInterest();
     }
