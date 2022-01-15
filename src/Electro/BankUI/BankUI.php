79173b9b678639ff001ffc749d1e1a5b9c0c7d5c
Keenan Yafiq <accbruh0@gmail.com>
2022-01-15 14:13:50 +0700

    Update BankUI.php

--- src/Electro/BankUI/BankUI.php
+++ src/Electro/BankUI/BankUI.php
@@ -90,7 +90,8 @@ class BankUI extends PluginBase implements Listener{
         switch($command->getName()){
             case "bank":
                 if($sender instanceof Player){
-                    if (isset($args[0]) && $sender->hasPermission("bankui.admin") || isset($args[0]) && $sender->isOp()){
+                    if (isset($args[0]) && $sender->hasPermission("bankui.admin")
+                       (isset($args[0]) && $sender->isOp()){
                         if (!file_exists($this->getDataFolder() . "Players/" . $args[0] . ".yml")){
                             $sender->sendMessage("§c§lError: §r§aThis player does not have a bank account");
                             return true;
