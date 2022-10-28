5e683906bdc485340815e9c7ae5e1065509cd7d2
Electro Games <2cool4u567@gmail.com>
2022-10-27 20:14:25 -0500

    Removed useless code

--- src/Electro/BankUI/ScoreHudTags.php
+++ src/Electro/BankUI/ScoreHudTags.php
@@ -2,21 +2,8 @@
 
 namespace Electro\BankUI;
 
-use cooldogedev\BedrockEconomy\libs\cooldogedev\libSQL\context\ClosureContext;
-use pocketmine\promise\PromiseResolver;
-use poggit\libasynql\libasynql;
-use poggit\libasynql\SqlError;
-use Vecnavium\FormsUI\SimpleForm;
-use Vecnavium\FormsUI\CustomForm;
-use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\player\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\utils\Config;
-use pocketmine\scheduler\ClosureTask;
 use Ifera\ScoreHud\event\TagsResolveEvent;
 use Ifera\ScoreHud\event\PlayerTagUpdateEvent;
 use Ifera\ScoreHud\scoreboard\ScoreTag;
